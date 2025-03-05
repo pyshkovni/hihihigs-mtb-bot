b815432fcc81112fabb0a780e66b72ab41bd1db3
pyshkovni <guess.the.tutor@gmail.com>
2025-03-05 14:30:34 +0300

    1. Развернули проект телеграм-бота на aiogram 2. Добавили два обработчика для старта бота

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+
+# Экземпляр бота и диспетчера
+bot = Bot(token="ВАШ ТОКЕН")
+dp = Dispatcher()
+
+# Бот принимает команды, например /start.
+# Создадим хендлер - обработчик сообщений, и будем возвращать сообщение
+@dp.message(Command('start'))
+async def process_start_command(message):
+    await message.answer("Привет!")
+
+
+@dp.message()
+async def echo_message(message):
+    await message.answer(message.text)
+
+# функция запуска проекта
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
